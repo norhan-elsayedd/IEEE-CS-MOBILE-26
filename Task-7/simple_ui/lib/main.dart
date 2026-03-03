7b4ff1c6a5b8d414589683500d9807b1b70a4e12
norhan-elsayed <ahmedelsayednn9@gmail.com>
2026-03-03 14:34:51 +0200

    add task 7

--- Task-7/simple_ui/lib/main.dart
+++ Task-7/simple_ui/lib/main.dart
@@ -4,31 +4,33 @@ void main() {
   runApp( MyApp());
 }
 class MyApp extends StatelessWidget{
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.black87,
+          backgroundColor: Colors.brown[900],
           elevation: 0,
           title: Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
-                 Text("Task 7",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.white,),),
+                 Text("Nina",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.white,),),
           Icon(Icons.favorite_border,color: Colors.white,),],
                ),
         ),
 
         body: Container(
+          color: Colors.brown[400],
           padding: EdgeInsets.all(10),
           child: Column(
             children: [
               Container(
                 padding: EdgeInsets.all(10),
                 decoration: BoxDecoration(
-                  color: Colors.blue[50],
+                  color: Colors.brown[100],
+                  borderRadius: BorderRadius.all(Radius.circular(30)),
                   border:Border.all(width: 2),
                 ),
                 child: Row(
@@ -36,15 +38,15 @@ class MyApp extends StatelessWidget{
                   children: [
                     Column(
                       children: [
-                        Icon(Icons.person,color: Colors.deepPurple,size: 60,weight:100 , )
+                        Icon(Icons.person,color: Colors.brown[700],size: 60,weight:100 , )
                       ],
                     ),
                     Padding(padding: EdgeInsets.all(10),
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          Text("Norhan Elsayed Atia", style: TextStyle(fontSize: 20,fontWeight: FontWeight.w900,color: Colors.black,),),
-                          Text("Flutter Developer",style: TextStyle(fontSize: 15,color:Colors.deepPurple ),),
+                          Text("Norhan Elsayed", style: TextStyle(fontSize: 20,fontWeight: FontWeight.w900,color: Colors.black,),),
+                          Text("Flutter Developer",style: TextStyle(fontSize: 15,color: Colors.brown[700] ),),
                         ],
                       ),
                     )
@@ -52,22 +54,24 @@ class MyApp extends StatelessWidget{
                 ),
               ),
               Container(
-                margin: EdgeInsets.only(top: 10),
+                margin: EdgeInsets.only(top: 8),
                 padding: EdgeInsets.symmetric(horizontal: 30,vertical: 20),
                 decoration: BoxDecoration(
-                  color: Colors.blue[50],
+                  color: Colors.brown[100],
+                  borderRadius: BorderRadius.all(Radius.circular(30)),
                   border:Border.all(width: 2),
                 ),
-                child: Text("What a glorious set of stairs we make!", textAlign: TextAlign.center ,
+                child: Text("What a glorious set of stairs we make!!", textAlign: TextAlign.center ,
                   style: 
                   TextStyle(color: Colors.black, fontSize:20, fontWeight:FontWeight.w600, fontStyle: FontStyle.italic ),
                 ),
               ),
 
                Container(
-                margin: EdgeInsets.only(top: 10),
+                margin: EdgeInsets.only(top: 8),
                 decoration: BoxDecoration(
-                  color: Colors.blue[50],
+                  color: Colors.brown[100],
+                  borderRadius: BorderRadius.all(Radius.circular(30)),
                   border:Border.all(width: 2),
 
                 ),
@@ -76,24 +80,25 @@ class MyApp extends StatelessWidget{
                   mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                   children: [
                     Row(children: [
-                      Icon(Icons.star , color: Colors.amber,size:25 ,),
-                      Icon(Icons.star , color: Colors.amber,size:25 ,),
-                      Icon(Icons.star , color: Colors.amber,size:25 ,),
-                      Icon(Icons.star , color: Colors.amber,size:25 ,),
-                      Icon(Icons.star_half , color: Colors.amber,size:25 ,),
+                      Icon(Icons.star_rounded , color: Colors.orange[100],size:25 ,),
+                      Icon(Icons.star_rounded , color: Colors.orange[100],size:25 ,),
+                      Icon(Icons.star_rounded , color: Colors.orange[100],size:25 ,),
+                      Icon(Icons.star_rounded , color: Colors.orange[100],size:25 ,),
+                      Icon(Icons.star_half_rounded , color: Colors.orange[100],size:25 ,),
                     ], 
                     ),
                     Text("150 Reviews",style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.w600)),
-                    Icon(Icons.share,size: 25,color: Colors.deepPurple,fontWeight: FontWeight.bold,)
+                    Icon(Icons.share,size: 25,color: Colors.brown[700],fontWeight: FontWeight.bold,)
                   ],
                 ),
               ),
 
                Container(
-                margin: EdgeInsets.only(top: 10),
+                margin: EdgeInsets.only(top: 8),
                 padding: EdgeInsets.all(10),
                 decoration: BoxDecoration(
-                  color: Colors.blue[50],
+                  color: Colors.brown[100],
+                  borderRadius: BorderRadius.all(Radius.circular(30)),
                   border:Border.all(width: 2),
 
                 ),
@@ -102,7 +107,7 @@ class MyApp extends StatelessWidget{
                   children: [
                     Column(
                       children: [
-                        Icon(Icons.code, color: Colors.deepPurple,fontWeight:FontWeight.bold , size: 25,),
+                        Icon(Icons.code, color: Colors.brown[700],fontWeight:FontWeight.bold , size: 25,),
                         Text('EXP:',style: TextStyle(fontWeight: FontWeight.w700, color: Colors.black,fontSize: 18),),
                         Container(height: 10,),
                         Text("1 week🤣",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 15))
@@ -110,15 +115,15 @@ class MyApp extends StatelessWidget{
                     ),
                     Column(
                       children: [
-                        Icon(Icons.bug_report_sharp,color: Colors.deepPurple,fontWeight:FontWeight.bold , size: 25,),
+                        Icon(Icons.bug_report_sharp,color: Colors.brown[700],fontWeight:FontWeight.bold , size: 25,),
                         Text('Bugs:',style: TextStyle(fontWeight: FontWeight.w700, color: Colors.black,fontSize: 18),),
                         Container(height: 10,),
-                        Text("😞جبنه اسطنبولى",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 15))
+                        Text("جبنه اسطنبولى",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 15))
                       ],
                     ),
                     Column(
                       children: [
-                        Icon(Icons.coffee,color: Colors.deepPurple,fontWeight:FontWeight.bold , size: 25,),
+                        Icon(Icons.coffee,color: Colors.brown[700],fontWeight:FontWeight.bold , size: 25,),
                         Text('Coffee:',style: TextStyle(fontWeight: FontWeight.w700, color: Colors.black,fontSize: 18),),
                         Container(height: 10,),
                         Text("∞",style: TextStyle(color: Colors.black,fontWeight: FontWeight.bold,fontSize: 15))
@@ -129,10 +134,11 @@ class MyApp extends StatelessWidget{
               ),
 
               Container(
-                margin: EdgeInsets.only(top: 10),
+                margin: EdgeInsets.only(top: 8),
                 padding: EdgeInsets.symmetric(horizontal: 12, vertical: 12),
                 decoration: BoxDecoration(
-                  color: Colors.blue[50], 
+                  color: Colors.brown[100],
+                  borderRadius: BorderRadius.all(Radius.circular(30)), 
                   border: Border.all(width: 2),
                 ),
                 child: Column(
@@ -141,9 +147,9 @@ class MyApp extends StatelessWidget{
                     Row(
                       crossAxisAlignment: CrossAxisAlignment.center,
                       children: [
-                        Icon(Icons.gps_fixed,color: Colors.red,size: 18,),
+                        Icon(Icons.gps_fixed,color: Colors.brown[800],size: 18,),
                         Container(width: 8), 
-                        Text("My Hobbies:",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.black,)),
+                        Text("My Hobbies:",style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold,color: Colors.brown[900],)),
                       ],
                     ),
                     Container(height: 5,),
